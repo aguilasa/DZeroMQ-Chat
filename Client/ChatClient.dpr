1c8c410e6910813f343a8b166e92177c45cd1fdc
aguilasa <ingmar.aguiar@gmail.com>
2017-12-06 19:27:50 -0200

    Inicial

---
+++ Client/ChatClient.dpr
@@ -0,0 +1,14 @@
+program ChatClient;
+
+uses
+  Vcl.Forms,
+  uPrincipal in 'uPrincipal.pas' {FPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFPrincipal, FPrincipal);
+  Application.Run;
+end.
